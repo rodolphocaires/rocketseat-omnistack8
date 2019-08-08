e46423c0d79f0a981ee64bfc2776dee0af335a57
Silva, Rodolpho Mendes Caires <Rodolpho.Silva@dieboldnixdorf.com>
2019-08-08 20:44:06 -0300

    frontend completo

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+import api from '../services/api';
+
+import logo from '../assets/logo.svg';
+
+export default function Login({ history }) {
+    const [username, setUsername] = useState('');
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        const body = {
+            username,
+        };
+
+        const response = await api.post('/devs', body);
+
+        const { _id } = response.data;
+
+        history.push(`/dev/${_id}`);
+    }
+
+    return (
+        <div className="login-container">
+            <form onSubmit={handleSubmit}>
+                <img src={logo} alt="Tindev" />
+                <input
+                    type="text"
+                    placeholder="Digite seu usuário no Github"
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}
+                />
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    );
+}
